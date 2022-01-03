a0840ea276091ea231238fa702566401eb3854f9
Paul Warta <paul@team-warta.de>
2022-01-03 16:02:35 +0100

    u

--- test.js
+++
@@ -1,3 +0,0 @@
-let string = `<?xml version="1.0" encoding="ISO-8859-1" ?><state><datapoint ise_id='1540' value='19.000000'/></state>`;
-let index = string.indexOf("value='");
-console.log(string.slice(string.indexOf("value='") + 7, string.indexOf("value='") + 11));
